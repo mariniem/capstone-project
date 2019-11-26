ff241682e70a78af0efd39f6fc2b8176da315bca
Marilena Niemann <mari_niemann@web.de>
2019-11-26 18:13:02 +0100

    Add description toggle logic

--- src/Exercise.js
+++ src/Exercise.js
@@ -5,45 +5,65 @@ import ExerciseButton from './ExerciseButton'
 import ArrowDown from './Icons/Arrow down.svg'
 import ArrowUp from './Icons/Arrow up.svg'
 
-export default function Exercise({
-  title,
-  description,
-  image,
-  isDescritionShown,
-}) {
+export default function Exercise({ title, description, image }) {
   const [isDescriptionShown, setIsDescriptionShwon] = useState(false)
 
   const onButtonClick = () => setIsDescriptionShwon(!isDescriptionShown)
   const button = isDescriptionShown ? ArrowDown : ArrowUp
-  /* const toggleDescription = (Title = { title }),
-    Description = { description } */
 
   return (
     <ExerciseWrapper>
       <ExerciseImages src={image} />
       <ExerciseButton src={button} onClick={onButtonClick}></ExerciseButton>
-      <DescriptionWrapper>
-        {isDescriptionShown ? description : ''}
-      </DescriptionWrapper>
+      {isDescriptionShown ? (
+        <DescriptionWrapper onButtonClick={!isDescriptionShown}>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+        </DescriptionWrapper>
+      ) : (
+        ''
+      )}
     </ExerciseWrapper>
   )
 }
 
 const DescriptionWrapper = styled.div`
-  background: white;
-  border: 2px solid #647d91;
+  /* background: white; */
+  /*  border: 2px solid #647d91; */
   position: absolute;
-  height: 80px;
+  height: auto;
   width: 150px;
-  bottom: -50px;
+  bottom: -100px;
   z-index: 100;
+  position: relative;
+  border-radius: 3px;
+  box-shadow: 0 5px 20px #0002;
+  background: #647d91;
+
+  &: after {
+    content: '';
+    position: absolute;
+    border: 10px solid transparent;
+    border-bottom-color: #647d91;
+    bottom: 100%;
+    left: 10%;
+  }
 `
 
 const Title = styled.p`
-  color: black;
+  color: #f2f2f2;
+  font-size: 12px;
+  font-weight: bold;
+  font-family: 'Nunito', sans-serif;
+  padding-top: 10px;
+  margin: 5px;
 `
 const Description = styled.p`
-  color: black;
+  color: #f2f2f2;
+  font-size: 10px;
+  font-family: 'Nunito', sans-serif;
+  margin: 5px;
+  padding: 5px;
 `
 
 const ExerciseWrapper = styled.section`
